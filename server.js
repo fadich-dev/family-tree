b6838beb4702a1bf5688e220beb1f7752c7f840b
fadi <fadich95@gmail.com>
2017-02-02 14:16:47 +0200

    ``` commit

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var express = require('express');
 var app = express();
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send('hello world');
 });
 
 
-app.all('*', function(req, res){
+app.all('*', function(req, res) {
     res.send("Incorrect request");
 });
 
