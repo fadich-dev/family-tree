68fd1ef4e80f9a9d00838ace2127095bc2d77536
fadi <fadich95@gmail.com>
2017-02-07 12:20:08 +0200

    ``` some

--- app/tree.component.ts
+++ app/tree.component.ts
@@ -1,13 +1,18 @@
-import {Component} from "angular2/core";
+import { Component }                        from "angular2/core";
+import { TreeService }                      from "./tree.service";
 
 @Component({
     selector: 'tree',
     template:
         `<main>
-            Tree
+            <div class="head">{{ title }}</div>
+            <div>{{ buildTree() }}</div>
         </main>`
 })
 
 export class TreeComponent {
-
+    title: string = "*** *** ***";
+    buildTree = function () {
+        return "";
+    }
 }
---
+++ app/tree.service.ts
@@ -0,0 +1,11 @@
+import { Injectable }       from 'angular2/core';
+
+export class TreeService {
+    getTree(): string[] {
+        return [
+            "qwe1",
+            "qwe22",
+            "qwe333",
+        ];
+    }
+}
