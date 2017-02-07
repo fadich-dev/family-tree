a21fecd5d44a062cebf898a0810b3fa40437134e
fadi <fadich95@gmail.com>
2017-02-07 11:23:46 +0200

    ``` some

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,14 @@ import {Component} from "angular2/core";
 
 @Component({
     selector: 'my-app',
-    template: '<header><div class="title"><h1 class="head">Family-tree</h1>' +
-    '<p class="footnote">Though it is more like a scheme of the network marketing</p></div></header>'
+    template:
+        `<header>
+            <div class="title">
+                <h1 class="head">Family-tree</h1>
+                <p class="footnote">Though it is more like a scheme of the network marketing</p>
+            </div>
+        </header>
+        <tree></tree>`
 })
 
 export class AppComponent { }
---
+++ app/tree.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector: 'tree',
+    template:
+        `<main>
+            Tree
+        </main>`
+})
+
+export class TreeComponent {
+
+}
