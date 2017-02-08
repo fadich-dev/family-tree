d3e8dabdb1e69d8cf289688139c743a69d498f0d
fadi <fadich95@gmail.com>
2017-02-08 14:11:48 +0200

    ``` build tree

--- models/tree.js
+++ models/tree.js
@@ -1,5 +1,7 @@
 var db = require('../db');
 
+var nodes = [];
+
 exports.getTree = function (callback) {
     try {
         db.get().collection('person').find().toArray(function (err, doc) {
@@ -7,10 +9,23 @@ exports.getTree = function (callback) {
                 console.error(err);
                 return callback(false);
             }
-            return callback(doc);
+
+            nodes = doc;
+            return callback(buildTree(null));
         });
     } catch (e) {
         console.error(e.message);
         return callback(false);
     }
 };
+
+function buildTree(parent) {
+    var tree = [];
+    nodes.forEach(function (element, index) {
+        if (element.parent == parent) {
+            tree[index] = element;
+            tree[index].children = buildTree(tree[index]._id);
+        }
+    });
+    return tree;
+}
